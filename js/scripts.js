251e51a1a20875beb1d86aca4943db45a958b511
NamesAreNotEasy <44278453+NamesAreNotEasy@users.noreply.github.com>
2021-06-03 21:53:19 -0400

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 var video = document.querySelector("#videoElement");
+
 if (navigator.mediaDevices.getUserMedia) {
   navigator.mediaDevices.getUserMedia({ video: true })
     .then(function (stream) {
@@ -8,6 +9,9 @@ if (navigator.mediaDevices.getUserMedia) {
       console.log("Something went wrong!");
     });
 }
+
+
+
 $(document).ready(function() {
 	$(document).on('keydown', function(event) {
 		event.preventDefault();
@@ -65,7 +69,10 @@ $(document).ready(function() {
 			$('.'+key).removeClass('press');
 			$('.'+key).addClass('active');
 		}
+
 	});
+
+
 $(document).on('keyup press', function(event) {
 		event.preventDefault();
 		var key;
@@ -114,10 +121,44 @@ $(document).on('keyup press', function(event) {
    		if (keyspressed === 0) $('.test').empty().append(allkeys);
    		// if (keyspressed === 10) audio.play();
 	});
+
 $(document).bind("contextmenu",function(e){
     	return false;
   	});
+
+
 });
+
+var OSName
+if (navigator.appVersion.indexOf("Win") !=-1) {
+	OSName="Windows";
+}
+else if (navigator.appVersion.indexOf("Mac") !=-1) {
+	OSName="MacOS";
+}
+else if (navigator.appVersion.indexOf("X11") !=-1) {
+	OSName="UNIX";
+}
+else if (navigator.appVersion.indexOf("Linux") !=-1) {
+	OSName="Linux";
+}
+
+var startkey="notsure";
+if (OSName==="Windows") startkey="windows";
+if (OSName==="MacOS") startkey="command";
+
+var altkey="notsure";
+if (OSName==="Windows") altkey="alt";
+if (OSName==="MacOS") altkey="option";
+
+var enterkey="notsure";
+if (OSName==="Windows") enterkey="enter"
+if (OSName==="MacOS") enterkey="return";
+
+var backkey="notsure";
+if (OSName==="Windows") backkey="backspace"
+if (OSName==="MacOS") backkey="delete";
+
 function toggle_visibility(id) {
        var e = document.getElementById(id);
        var f = $('.container');
@@ -135,29 +176,49 @@ function toggle_visibility(id) {
           h.css("width", "900");
           g.empty().append('Show Numpad');
        }
+
    }
+
+
    var record = document.querySelector('.record');
    var stop = document.querySelector('.stop');
    var soundClips = document.querySelector('.sound-clips');
    var canvas = document.querySelector('.visualizer');
    var mainSection = document.querySelector('.main-controls');
+
+   // disable stop button while not recording
+
    stop.disabled = true;
+
+   // visualiser setup - create canvas
+
    var canvasCtx = canvas.getContext("2d");
+
+   //main block for doing the audio recording
+
    if (navigator.mediaDevices.getUserMedia) {
 	 console.log('getUserMedia supported.');
+
 	 var constraints = { audio: true };
 	 var chunks = [];
+
 	 var micName = '';
 	 var onSuccess = function(stream) {
 	   var mediaRecorder = new MediaRecorder(stream);
+
 	   var audioSourceInfo = stream.getAudioTracks()[0];
+
 	   micName = audioSourceInfo.label;
 	   document.querySelector('.current-microphone').textContent = "Current microphone: \"" + micName + "\"";
+
 	   visualize(stream);
+
 	   record.addEventListener("click", startRecording);
 	   record.addEventListener("touchstarat", startRecording);
+
 	   stop.addEventListener("click", stopRecording);
 	   stop.addEventListener("touchend", stopRecording);
+
 	   function startRecording() {
 		 mediaRecorder.start();
 		 console.log(mediaRecorder.state);
@@ -167,50 +228,142 @@ function toggle_visibility(id) {
 		 stop.disabled = false;
 		 record.disabled = true;
 	   }
+
+
+	   function stopRecording() {
+		 mediaRecorder.stop();
+		 console.log(mediaRecorder.state);
+		 console.log("recorder stopped");
+		 record.style.background = "";
+		 record.style.color = "";
+		 // mediaRecorder.requestData();
+
+		 stop.disabled = true;
+		 record.disabled = false;
+	   }
+
+	   var clipCount = 0;
+	   mediaRecorder.onstop = function(e) {
+		 clipCount +=1;
+		 console.log("data available after MediaRecorder.stop() called.");
+
+		 var clipName = 'clip ' + clipCount + ' recorded using "' + micName + '"';
+		 console.log(clipName);
+		 var clipContainer = document.createElement('article');
+		 var clipLabel = document.createElement('p');
+		 var audio = document.createElement('audio');
+		 var deleteButton = document.createElement('button');
+
+		 clipContainer.classList.add('clip');
+		 audio.setAttribute('controls', '');
+		 deleteButton.textContent = 'Delete';
+		 deleteButton.className = 'delete';
+
+		 if(clipName === null) {
+		   clipLabel.textContent = 'My unnamed clip';
+		 } else {
+		   clipLabel.textContent = clipName;
+		 }
+
+		 clipContainer.appendChild(audio);
+		 clipContainer.appendChild(clipLabel);
+		 clipContainer.appendChild(deleteButton);
+		 soundClips.appendChild(clipContainer);
+
+		 audio.controls = true;
+		 var blob = new Blob(chunks, { 'type' : 'audio/ogg; codecs=opus' });
+		 chunks = [];
+		 var audioURL = window.URL.createObjectURL(blob);
+		 audio.src = audioURL;
+		 console.log("recorder stopped");
+
+		 deleteButton.onclick = function(e) {
+		   evtTgt = e.target;
+		   evtTgt.parentNode.parentNode.removeChild(evtTgt.parentNode);
+		 }
+
+		 clipLabel.onclick = function() {
+		   var existingName = clipLabel.textContent;
+		   var newClipName = prompt('Enter a new name for your sound clip?');
+		   if(newClipName === null) {
+			 clipLabel.textContent = existingName;
+		   } else {
+			 clipLabel.textContent = newClipName;
+		   }
+		 }
+	   }
+
+	   mediaRecorder.ondataavailable = function(e) {
+		 chunks.push(e.data);
+	   }
 	 }
+
 	 var onError = function(err) {
 	   console.log('The following error occured: ' + err);
 	 }
+
 	 navigator.mediaDevices.getUserMedia(constraints).then(onSuccess, onError);
+
    } else {
 	  console.log('getUserMedia not supported on your browser!');
    }
+
    function visualize(stream) {
 	 var audioCtx = new (window.AudioContext || webkitAudioContext)();
 	 var source = audioCtx.createMediaStreamSource(stream);
+
 	 var analyser = audioCtx.createAnalyser();
 	 analyser.fftSize = 2048;
 	 var bufferLength = analyser.frequencyBinCount;
 	 var dataArray = new Uint8Array(bufferLength);
+
 	 source.connect(analyser);
+	 //analyser.connect(audioCtx.destination);
+
 	 draw()
+
 	 function draw() {
 	   WIDTH = canvas.width
 	   HEIGHT = canvas.height;
+
 	   requestAnimationFrame(draw);
+
 	   analyser.getByteTimeDomainData(dataArray);
+
 	   canvasCtx.fillStyle = 'rgb(200, 200, 200)';
 	   canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
 	   canvasCtx.lineWidth = 2;
 	   canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
+
 	   canvasCtx.beginPath();
+
 	   var sliceWidth = WIDTH * 1.0 / bufferLength;
 	   var x = 0;
+
+
 	   for(var i = 0; i < bufferLength; i++) {
+
 		 var v = dataArray[i] / 128.0;
 		 var y = v * HEIGHT/2;
+
 		 if(i === 0) {
 		   canvasCtx.moveTo(x, y);
 		 } else {
 		   canvasCtx.lineTo(x, y);
 		 }
+
 		 x += sliceWidth;
 	   }
+
 	   canvasCtx.lineTo(canvas.width, canvas.height/2);
 	   canvasCtx.stroke();
+
 	 }
    }
+
    window.onresize = function() {
 	 canvas.width = mainSection.offsetWidth;
    }
+
    window.onresize();
